980445f9ba65d296cd802f989c379b1645a0e8a2
Aleksander <olek_mazur@wp.pl>
2021-05-23 12:47:21 +0200

    poczatkowe wczytanie danych

---
+++ czesc_programistyczna/metoda_forda_fulkersona/algorytm.py
@@ -0,0 +1,37 @@
+import json
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+blad = True
+while blad:
+    try:
+        plik = input("Podaj nazwe pliku z danymi: ")
+        with open(plik) as json_file:
+            m_sasiedztwa = json.load(json_file)
+            blad = False
+    except FileNotFoundError:
+        print("Nie ma takiego pliku, sprobuj ponownie")
+        blad = True
+
+m_sasiedztwa = np.array(m_sasiedztwa)
+print(m_sasiedztwa)
+
+graf = nx.DiGraph()
+graf.add_nodes_from(range(m_sasiedztwa.shape[0]))
+print(graf.nodes)
+
+for i in range(m_sasiedztwa.shape[0]):
+    for j in range(m_sasiedztwa.shape[1]):
+        if m_sasiedztwa[i][j] != 0:
+            graf.add_weighted_edges_from([(i, j, m_sasiedztwa[i][j])])
+
+print(graf.edges)
+# print(graf.successors())
+nx.draw_spectral(graf, with_labels=True, font_weight='bold')
+# plt.subplot(122)
+# nx.draw_shell(graf, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
+plt.show()
+
+
+
